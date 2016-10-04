8f8c40b2ac155304f92449194fe3de7b51130a9b
mthomsit <mark@makeandship.co.uk>
2016-10-04 09:38:10 +0100

    Manage outside Settings/Options API to interact with multisite options

---
+++ src/html-utils.php
@@ -0,0 +1,111 @@
+<?php
+
+class HtmlUtils {
+	protected function __construct() {
+
+	}
+
+	public static function render_field( $label, $name, $args ) {
+		$html = [];
+
+		$type = isset($args['type']) ? $args['type'] : 'text';
+
+		if (isset($type) && !empty($type)) {
+			$field = '';
+
+			switch($type) {
+				case 'text': 
+					$field = self::render_text_field( $name, $args );
+					break;
+			}
+
+			$html = [
+				'<div class="acf-elasticsearch-row">',
+				'	<div class="acf-elasticsearch-label">',
+				'		<label for="">'.$label.'</label>',
+			    '	</div>',
+				'	<div class="acf-elasticsearch-field">',
+				'		'.$field,
+				'	</div>',
+				'</div>'
+			];
+		}
+
+		return implode( $html, PHP_EOL );
+	}
+
+	public static function render_text_field( $name, $args ) {
+		$value = null;
+
+		if (is_multisite()) { 
+			$option = get_site_option( $name );
+		}
+		else {
+			$option = '';
+		}
+
+		if (isset($option) && empty($option)) {
+			$value = $option;
+		}
+		else {
+			if (array_key_exists('value', $args)) {
+				$value = $args['value'];
+			}
+		}
+
+		$clazz = isset($args) && array_key_exists('class', $args) ? $args['class'] : '';
+
+		$html = [
+			'<input type="text" ',
+			'	class="'.$clazz.'"',
+			'	name="'.$name.'"',
+			'	value="'.$value.'"'
+		];
+		
+		unset($args['value']);
+		unset($args['class']);
+
+		foreach($args as $key => $value) {
+			$html[] = '	'.$key.'="'.$value.'"';
+		}
+
+		$html[] = '/>';
+		
+
+		return implode( $html, PHP_EOL );
+	}
+
+	public static function render_buttons( $buttons ) {
+
+		$html = [];
+
+		$html[] = '<div class="acf-elasticsearch-row">';
+		$html[] = '	<div class="acf-elasticsearch-button-container">';
+
+		foreach($buttons as $button) {
+			$html[] = self::render_button( $button );
+		}
+
+		$html[] = '	</div>';
+		$html[] = '</div>';
+
+		return implode( $html, PHP_EOL );
+	}
+
+	public static function render_button( $args ) {
+		
+		$html = [
+			'<input type="submit" '
+		];
+
+		foreach( $args as $key => $value ) {
+			$html[] = '	'.$key.'="'.$value.'"';
+		}
+			
+		$html[] = '/>';
+		
+
+		return implode( $html, PHP_EOL );
+	}
+}
+
--- src/wp/admin/settings.php
+++ src/wp/admin/settings.php
@@ -1,23 +1,105 @@
 <?php
+require_once plugin_dir_path(__DIR__).'../html-utils.php';
 ?>
 <div class="wrap">
-<?php screen_icon();?>
-<form id="acf_elasticsearch_options" action="options.php" method="post">
 <?php
-	do_settings_sections( 'acf_elasticsearch_settings_page' );
-	submit_button('Save', 'primary', 'acf_elasticsearch_options_submit');
-
-	do_settings_sections( 'acf_elasticsearch_mappings_page' );
-	submit_button('Create Mappings', '', 'acf_elasticsearch_mappings_submit');
-
-	do_settings_sections( 'acf_elasticsearch_index_page' ); ?>
-	<div class="acf-elasticsearch-button-container">
-	<?php
-	submit_button('Index Posts', '', 'acf_elasticsearch_index_submit');
-	submit_button('Clear Index', '', 'acf_elasticsearch_clear_index_submit');
-	?>
-	</div>
-	<?php
+	if ( !empty( $_POST ) ) {
+		// save incoming options
+		error_log('save post data');
+	}
 ?>
- </form>
+<h1>ACF Elasticsearch</h1>
+	<div id="poststuff">
+		<form method="post" action="">
+			<div class="postbox">
+				<h2 class="hndle"><span>1. Configure your Elastic search server</span></h2>
+				<div class="inside acf-fields -left">
+					<?php 
+					echo HtmlUtils::render_field(
+						'Server', 
+						'acf_elasticsearch_server',
+						array(
+							'class' => 'long',
+							'placeholder' => 'e.g. http://www.yourserver.com:9200/'
+						)
+					);
+					echo HtmlUtils::render_field(
+						'Primary Index', 
+						'acf_elasticsearch_primary_index',
+						array(
+							'class' => '',
+							'placeholder' => ''
+						)
+					); 
+					echo HtmlUtils::render_field(
+						'Secondary Index', 
+						'acf_elasticsearch_secondary_index',
+						array(
+							'class' => '',
+							'placeholder' => ''
+						)
+					); 
+					echo HtmlUtils::render_field(
+						'Read Timeout', 
+						'acf_elasticsearch_read_timeout',
+						array(
+							'class' => 'short',
+							'placeholder' => '',
+							'value' => 30
+						)
+					); 
+					echo HtmlUtils::render_field(
+						'Write Timeout', 
+						'acf_elasticsearch_write_timeout',
+						array(
+							'class' => 'short',
+							'placeholder' => '',
+							'value' => 30
+						)
+					); 
+					echo HtmlUtils::render_buttons([
+						array(
+							'value' => 'Save', 
+							'name' => 'acf_elasticsearch_save_button',
+							'class' => 'button-primary'
+						)
+					]); 
+					?>
+				</div>
+			</div>
+			<div class="postbox">
+				<h2><span>2. Setup server mappings</span></h2>
+				<div class="inside acf-fields -left">
+				<?php
+					echo HtmlUtils::render_buttons([
+						array(
+							'value'=>'Create mappings', 
+							'name' => 'acf_elasticsearch_create_mappings_button',
+							'class' => 'button'
+						)
+					]); 
+				?>
+				</div>
+			</div>
+			<div class="postbox">
+				<h2><span>3. Index the data</span></h2>
+				<div class="inside acf-fields -left">
+				<?php
+					echo HtmlUtils::render_buttons([
+						array(
+							'value' => 'Index posts', 
+							'name' => 'acf_elasticsearch_index_posts_button',
+							'class' => 'button'
+						),
+						array(
+							'value' => 'Clear index', 
+							'name' => 'acf_elasticsearch_clear_index_button',
+							'class' => 'button'
+						)
+					]); 
+				?>
+				</div>
+			</div>
+	 	</form>
+	</div>
 </div>
