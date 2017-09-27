3bcf67e55883a61440ef40826df549231dd74430
Mark Thomsit <mark@ceremium.com>
2017-09-27 12:16:26 +0100

    Use autoloader

--- tests/makeandship/elasticsearch/TransformersTest.php
+++ tests/makeandship/elasticsearch/TransformersTest.php
@@ -1,9 +1,14 @@
 <?php
 
+require_once(__DIR__ . '/../../../acf-elasticsearch-autoloader.php');
+
+use makeandship\elasticsearch\transformer\DateFieldTransformer;
+
 class TransformersTest extends \PHPUnit\Framework\TestCase
 {
     public function testTrueIsTrue()
     {
+    	$transformer = new DateFieldTransformer();
         $foo = true;
         $this->assertTrue($foo);
     }
