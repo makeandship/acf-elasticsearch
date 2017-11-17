d734f73b6a3d47f6120b3d1118a0a1f5407a9228
Mark Thomsit <mark@ceremium.com>
2017-11-17 11:18:04 +0000

    Move username and password to settings

--- src/makeandship/elasticsearch/constants.php
+++ src/makeandship/elasticsearch/constants.php
@@ -2,41 +2,46 @@
 
 namespace makeandship\elasticsearch;
 
-class Constants {
+class Constants
+{
 
-	// index 
-	const DEFAULT_SHARDS = 1;
-	const DEFAULT_REPLICAS = 1;
+    // index
+    const DEFAULT_SHARDS = 1;
+    const DEFAULT_REPLICAS = 1;
 
-	// elastica
-	const SETTING_URL = 'url';
-	const SETTING_TIMEOUT = 'timeout';
-	const SETTING_USERNAME = 'username';
-	const SETTING_PASSWORD = 'password';
+    // elastica
+    const SETTING_URL = 'url';
+    const SETTING_TIMEOUT = 'timeout';
+    const SETTING_USERNAME = 'username';
+    const SETTING_PASSWORD = 'password';
 
-	const DEFAULT_WRITE_TIMEOUT = 30;
-	const DEFAULT_READ_TIMEOUT = 30;
+    const DEFAULT_WRITE_TIMEOUT = 30;
+    const DEFAULT_READ_TIMEOUT = 30;
 
-	// plugin
-	const VERSION = '0.0.1';
-	const DB_VERSION = 1;
+    // plugin
+    const VERSION = '0.0.1';
+    const DB_VERSION = 1;
 
-	const OPTION_SERVER = 'acf_elasticsearch_server';
-	const OPTION_PRIMARY_INDEX = 'acf_elasticsearch_primary_index';
-	const OPTION_SECONDARY_INDEX = 'acf_elasticsearch_secondary_index';
-	const OPTION_READ_TIMEOUT = 'acf_elasticsearch_read_timeout';
-	const OPTION_WRITE_TIMEOUT = 'acf_elasticsearch_write_timeout';
-	const OPTION_INDEX_STATUS = 'acf_elasticsearch_index_status';
+    const OPTION_SERVER = 'acf_elasticsearch_server';
+    const OPTION_PRIMARY_INDEX = 'acf_elasticsearch_primary_index';
+    const OPTION_SECONDARY_INDEX = 'acf_elasticsearch_secondary_index';
+    const OPTION_READ_TIMEOUT = 'acf_elasticsearch_read_timeout';
+    const OPTION_WRITE_TIMEOUT = 'acf_elasticsearch_write_timeout';
+    const OPTION_INDEX_STATUS = 'acf_elasticsearch_index_status';
+    const OPTION_USERNAME = 'acf_elasticsearch_username';
+    const OPTION_PASSWORD = 'acf_elasticsearch_password';
 
-	// indexer
-	const STATUS_PUBLISH = 'publish';
-	const INDEX_POST_STATUSES = [Constants::STATUS_PUBLISH];
+    // indexer
+    const STATUS_PUBLISH = 'publish';
+    const INDEX_POST_STATUSES = [Constants::STATUS_PUBLISH];
 
-	// TODO move to settings
-	const DEFAULT_POSTS_PER_PAGE = 50;
-	const DEFAULT_TERMS_PER_PAGE = 50;
-	const DEFAULT_SITES_PER_PAGE = 50;
+    // TODO move to settings
+    const DEFAULT_POSTS_PER_PAGE = 50;
+    const DEFAULT_TERMS_PER_PAGE = 50;
+    const DEFAULT_SITES_PER_PAGE = 50;
 
-	// no instantiation
-	protected function __construct() {}
+    // no instantiation
+    protected function __construct()
+    {
+    }
 }
