56ba8d87a0bff0120f234781ba537063e4ff28dc
Mark Thomsit <mark@ceremium.com>
2017-11-17 13:06:01 +0000

    Capture value and suggest value

--- src/makeandship/elasticsearch/term-document-builder.php
+++ src/makeandship/elasticsearch/term-document-builder.php
@@ -2,51 +2,55 @@
 
 namespace makeandship\elasticsearch;
 
-class TermDocumentBuilder extends DocumentBuilder {
-	
-	/**
-	 * 
-	 */
-	public function build( $term ) {
-		$document = null;
+class TermDocumentBuilder extends DocumentBuilder
+{
+    
+    /**
+     *
+     */
+    public function build($term)
+    {
+        $document = null;
 
-		if (isset( $term )) {
-			foreach(TermMappingBuilder::CORE_FIELDS as $name => $definition) {
-				$value = $term->{$name};
+        if (isset($term)) {
+            foreach (TermMappingBuilder::CORE_FIELDS as $name => $definition) {
+                $value = $term->{$name};
 
-				// transform if required
-				if (is_array($definition) && array_key_exists('transformer', $definition)) {
-					$transformer = new $definition['transformer'];
-					
-					if (isset($transformer)) {
-						$value = $transformer->transform($value);
-					}
+                // transform if required
+                if (is_array($definition) && array_key_exists('transformer', $definition)) {
+                    $transformer = new $definition['transformer'];
+                    
+                    if (isset($transformer)) {
+                        $value = $transformer->transform($value);
+                    }
+                }
+                
+                $document[$name] = $value;
+                
+                if (is_array($definition) && array_key_exists('suggest', $definition)) {
+                    if ($definition['suggest']) {
+                        $document[$name.'_suggest'] = $value;
+                    }
+                }
+            }
+        }
 
-					$document[$name] = $value; 
-				}
+        return $document;
+    }
 
-				if (is_array($definition) && array_key_exists('suggest', $definition)) {
-					if ($definition['suggest']) {
-						$document[$name.'_suggest'] = $value;
-					}
-				}
-			}
-		}
+    /**
+     *
+     */
+    public function get_id($term)
+    {
+        return $term->term_id;
+    }
 
-		return $document;
-	}
-
-	/**
-	 *
-	 */
-	public function get_id( $term ) {
-		return $term->term_id;
-	}
-
-	/**
-	 *
-	 */
-	public function get_type( $term ) {
-		return $term->taxonomy;
-	}
+    /**
+     *
+     */
+    public function get_type($term)
+    {
+        return $term->taxonomy;
+    }
 }
