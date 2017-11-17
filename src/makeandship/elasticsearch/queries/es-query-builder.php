adb09582b88123517fc1927b7981c88621cc5c2b
Yassire Elhani <yassire@makeandship.com>
2017-11-17 16:55:17 +0000

    add query-builder class

---
+++ src/makeandship/elasticsearch/queries/es-query-builder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace makeandship\elasticsearch\queries;
+
+class ESQueryBuilder {	
+    private $query;
+    
+    public function __construct()
+    {
+        $this->query = "";
+    }
+
+    public function query($fields)
+    {
+        // implement fields query
+        return $this;
+    }
+
+    public function fuzziness($value)
+    {
+        // add fuzziness
+        return $this;
+    }
+
+    public function limit($value)
+    {
+        // add limit
+        return $this->str;
+    }
+
+    public function sort($field)
+    {
+        // add sort
+        return $this->str;
+    }
+}
