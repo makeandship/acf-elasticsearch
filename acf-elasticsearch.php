01cd1e2df65b2dac4bb0856676597461953db6fa
Yassire Elhani <yassire@makeandship.com>
2021-10-14 11:52:29 +0100

    add ms logging lib

--- acf-elasticsearch.php
+++ acf-elasticsearch.php
@@ -3,7 +3,7 @@
 Plugin Name: ACF Elasticsearch
 Plugin URI:  https://www/makeandship.com/blog/acf-elasticsearch
 Description: Elasticsearch integration for ACF-based wordpress sites
-Version:     7.4.8
+Version:     7.4.9
 Author:      Make and Ship Limited
 Author URI:  https://www.makeandship.com/
 License:     MIT
